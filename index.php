0335639d6628dec127408c5a74165e7f78ed56de
Esteban Villantoy <esteban.villantoy@touchconsulting.pe>
2024-05-20 02:33:56 -0500

    fix: env

--- index.php
+++ index.php
@@ -5,14 +5,15 @@ define('BASE_PATH', __DIR__ . '/');
 
 require_once BASE_PATH . 'vendor/autoload.php';
 
-use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__); 
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
 $dotenv->load();
 
 require_once BASE_PATH . 'src/orm/bootstrap.php';
 // routes
 require_once BASE_PATH . 'src/routes/user.route.php';
 require_once BASE_PATH . 'src/routes/login.route.php';
+
 
 Flight::start();
