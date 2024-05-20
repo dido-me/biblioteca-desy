f337ba88f6b3d6ce93424e0860d4d79a3119bea7
Esteban Villantoy <esteban.villantoy@touchconsulting.pe>
2024-05-20 02:45:30 -0500

    --

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
 
 declare(strict_types=1);
+
 define('BASE_PATH', __DIR__ . '/');
 
 require_once BASE_PATH . 'vendor/autoload.php';
 
+use Dotenv\Dotenv;
 
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
+// Cargar las variables de entorno
+$dotenv = Dotenv::createImmutable(BASE_PATH); 
 $dotenv->load();
 
+// Verifica que las variables de entorno críticas estén definidas
+$required_env_vars = ['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS'];
+foreach ($required_env_vars as $env_var) {
+    if (!isset($_ENV[$env_var])) {
+        die("Error: Missing required environment variable $env_var");
+    }
+}
+
 require_once BASE_PATH . 'src/orm/bootstrap.php';
-// routes
+
+// Routes
 require_once BASE_PATH . 'src/routes/user.route.php';
 require_once BASE_PATH . 'src/routes/login.route.php';
 
-
+// Iniciar la aplicación FlightPHP
 Flight::start();
