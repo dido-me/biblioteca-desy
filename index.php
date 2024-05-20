fcdf042be5ba683d5c4d787ad697cb302182b88a
Esteban Villantoy <esteban.villantoy@touchconsulting.pe>
2024-05-20 03:13:11 -0500

    fix: config envs

--- index.php
+++ index.php
@@ -6,19 +6,15 @@ define('BASE_PATH', __DIR__ . '/');
 
 require_once BASE_PATH . 'vendor/autoload.php';
 
-// use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
-// // Cargar las variables de entorno
-// $dotenv = Dotenv::createImmutable(BASE_PATH); 
-// $dotenv->load();
+$env = $_ENV['APP_ENV'] ?: 'production';
 
-// // Verifica que las variables de entorno críticas estén definidas
-// $required_env_vars = ['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS'];
-// foreach ($required_env_vars as $env_var) {
-//     if (!isset($_ENV[$env_var])) {
-//         die("Error: Missing required environment variable $env_var");
-//     }
-// }
+if ($env === 'development') {
+    $dotenv = Dotenv::createImmutable(BASE_PATH);
+    $dotenv->load();
+}
+
 
 require_once BASE_PATH . 'src/orm/bootstrap.php';
 
